4adfc45acdccb2e1943ccea39385cde8b893198d
Damaris Lopez <damaris.lopez@epn.edu.ec>
2024-11-11 23:13:29 -0500

    Update Main.java

--- Grupo2/src/Main.java
+++ Grupo2/src/Main.java
@@ -2,12 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        //Puedo editar este archivo
+        //Tiro parabolico
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        for (int y = 1; y <= 5; y++) {
+            //Esta en otroidioma
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
